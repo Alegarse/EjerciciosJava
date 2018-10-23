7440445588d425cac4b4b6eb00aa6580ffea10b1
Alejandro García Serrano <agscm2@gmail.com>
2018-10-23 23:15:52 +0200

    Subida Ejercicio 4

---
+++ Ejercicio4.java
@@ -0,0 +1,22 @@
+/** Ejercicios de Java
+*
+* Ejercicio 4 de la colección
+*
+* Autor: Alejandro  García Serrano
+*/
+
+
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    System.out.println("Horario de Clase");
+    System.out.println();
+    System.out.println("HORARIO   Lunes  Martes  Miércoles  Jueves  Viernes");
+    System.out.println("---------------------------------------------------");
+    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"PRIMERA","EED","SINF","PRO","PRO","SINF");
+    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"SEGUNDA","LM","SINF","PRO","PRO","SINF");
+    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"TERCERA","LM","SINF","PRO","EED","SINF");
+    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"CUARTA","BBDD","PRO","BBDD","EED","FOL");
+    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"QUINTA","BBDD","PRO","BBDD","LM","FOL");
+    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"SEXTA","BBDD","PRO","BBDD","LM","FOL");
+  }
+}
