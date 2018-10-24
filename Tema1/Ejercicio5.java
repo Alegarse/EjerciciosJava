741e0086d6082dc48a250390c08b58642a2a7bea
Alejandro García Serrano <agscm2@gmail.com>
2018-10-24 18:07:15 +0200

    Correción

--- Tema1/Ejercicio5.java
+++ Tema1/Ejercicio5.java
@@ -9,23 +9,16 @@
 public class Ejercicio5 {
   public static void main(String[] args) {
 
-    String rojo ="\033[31m";
-    String verde ="\033[32m";
-    String naranja ="\033[33m";
-    String azul ="\033[34m";
-    String morado ="\033[35m";
-    String blanco ="\033[37m";
-
     System.out.println("Horario de Clase");
     System.out.println();
     System.out.println("HORARIO   Lunes  Martes  Miércoles  Jueves  Viernes");
     System.out.println("---------------------------------------------------");
-    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"PRIMERA",rojo + "EED",verde + "INF",naranja + "PRO",naranja + "PRO",verde + "SINF" + blanco);
-    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"SEGUNDA",morado + "LM",verde + "SINF",naranja + "PRO",naranja + "PRO",verde + "SINF" + blanco);
-    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"TERCERA",morado + "LM",verde + "SINF",naranja + "PRO",rojo + "EED",verde + "SINF" + blanco);
-    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"CUARTA",blanco + "BBDD",naranja + "PRO",blanco + "BBDD",rojo + "EED",azul + "FOL" + blanco);
-    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"QUINTA",blanco + "BBDD",naranja + "PRO",blanco + "BBDD",morado + "LM",azul + "FOL" + blanco);
-    System.out.printf("%-8s  %-5s  %-6s  %-9s  %-6s  %-7s\n" ,"SEXTA",blanco + "BBDD",naranja + "PRO",blanco + "BBDD",morado + "LM",azul + "FOL" + blanco);
-    System.out.println("---------------------------------------------------");
+    System.out.println("PRIMERA   \033[31mEED    \033[32mINF     \033[33mPRO        \033[33mPRO     \033[32mSINF");
+    System.out.println("\033[37mSEGUNDA   \033[35mLM     \033[32mINF     \033[33mPRO        \033[33mPRO     \033[32mSINF");
+    System.out.println("\033[37mTERCERA   \033[35mLM     \033[32mSINF    \033[33mPRO        \033[31mEED     \033[32mSINF");
+    System.out.println("\033[37mCUARTA    \033[37mBBDD   \033[33mPRO     \033[37mBBDD       \033[31mEED     \033[34mFOL");
+    System.out.println("\033[37mQUINTA    \033[37mBBDD   \033[33mPRO     \033[37mBBDD       \033[35mLM      \033[34mFOL");
+    System.out.println("\033[37mSEXTA     \033[37mBBDD   \033[33mPRO     \033[37mBBDD       \033[35mLM      \033[34mFOL");
+    System.out.println("\033[37m---------------------------------------------------");
   }
 }
